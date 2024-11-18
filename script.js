4970b1c19a9602df947aabea31994324ceeef87b
m-akif-h <chaudharyakifmnbrands@gmail.com>
2024-11-18 02:08:53 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,119 @@
+// let name = prompt ("Enter your Name");
+
+//  if  (name % 5 ===0){
+// console.log("yes")
+//  }
+//  else {
+//     console.log("no")
+//  }
+
+
+// let score = prompt ("enter your score");
+// let grade = ""
+
+// if (score>=80  &&  score<=100) {
+//     console.log("A")
+// }
+// else if ( (score>=70  &&  score<=79) )
+//     {
+//         console.log("B") }
+        
+// else if ( (score>=60  &&  score<=69) )
+//     {
+//         console.log("C") }
+        
+// else if ( (score>=50  &&  score<=59) )
+//     {
+//         console.log("D") }
+        
+// else if ( (score>= 40  &&  score<=49) )
+//     {
+//         console.log("E") }
+
+        
+// else if ( (score>=30  &&  score<=39) )
+//     {
+//          console.log("You are fail try next year")
+//          alert ( "You are fail try next year")  
+//      }
+//      else  
+//         {
+//              console.log("You are fail try next year")
+//              alert ( "You are fail try next year")  
+//          }
+ 
+//  console.log is in one line 
+
+
+// let score = prompt ("enter your score");
+// let grade = ""
+
+// if (score>=80  &&  score<=100) 
+//     {
+//         grade ="A"
+//     }
+//     else if ( score>=70  &&  score<=79 )
+// {
+//     grade= "B"
+// }
+        
+// else if ( score>=60  &&  score<=69 )
+//     {
+//         grade = "C"
+//     }       
+// else if ( score>=50  &&  score<=59 )
+//     {
+//         grade ="D" }
+        
+// else if ( score>= 40  &&  score<=49) 
+//     {
+//         grade="E" }
+
+        
+// else if ( score>=30  &&  score<=39) 
+//     { 
+
+// grade="You are fail try next year"
+//          alert ( "You are fail try next year")  
+//      }
+//      else  
+//         {
+//              grade="You are fail try next year" 
+//              alert ( "You are fail try next year")  
+//          }
+
+//          console.log ("according your score your grade is " , grade) ;
+//          alert ( grade )
+
+
+
+// for loop
+// for  (let count = 1 ; count<=5 ; count++  ){
+//     console.log ("akif h")
+// }   
+
+
+
+//  let num = 0;
+//  for ( i = 1; i<=25; i++){
+//     num =num+ i;
+//  }
+//  console.log ( "sum" , num)
+
+
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed.";
+//   } 
+
+
+//  for of loop 
+
+
+let str = "muhammad akif hussain";
+let size = 0 ;
+for (let  i of str ){
+    console.log ( i)
+ size++ 
+}
+console.log ("size of" , size )
